d01d4fca0e3d2f2075d1bcf3302597c93462419f
PRANAV SHILU <pranavshilu1811@gmail.com>
2025-12-13 22:45:11 +0530

    Day 1 12/12/2025

--- src/assets/V13.jsx
+++ src/assets/V13.jsx
@@ -2,13 +2,12 @@ export default function V13() {
   function fruit(name) {
     alert(name);
   }
+
   return (
-    <>
-      <section>
-        <h3>V13</h3>
-        <button onClick={() => fruit("Apple")}>Apple</button>
-        <button onClick={() => fruit("Banana")}>Banana</button>
-      </section>
-    </>
+    <section>
+      <h3>V13</h3>
+      <button onClick={() => fruit("Apple")}>Apple</button>
+      <button onClick={() => fruit("Banana")}>Banana</button>
+    </section>
   );
 }
